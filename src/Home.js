09fd8e34b63b55dfad3dd1708f59999e77497db4
Akshara Mishra <akshara20@navgurukul.org>
2022-07-20 11:25:26 +0530

    useNavigate, useParams, dynamic routing, react-router-dom

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,29 @@
 import React from "react";
 // import {Link} from 'react-router-dom'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    const navigate = useNavigate();
-    
-    const aboutpage =() =>{
-        navigate('/about')
-    }
+  const navigate = useNavigate();
 
-    const contactpage =() =>{
-        navigate('/contact')
-    }
+  const aboutpage = () => {
+    navigate("/about");
+  };
 
+  const contactpage = () => {
+    navigate("/contact");
+  };
 
   return (
     <div>
       <h1>This is the home page</h1>
-     <button onClick={ () => aboutpage ()}> Click to view our about page </button>
-     <button onClick={ () => contactpage ()}> Click to view our contact page </button>
+      <button onClick={() => aboutpage()}>
+        {" "}
+        Click to view our about page{" "}
+      </button>
+      <button onClick={() => contactpage()}>
+        {" "}
+        Click to view our contact page{" "}
+      </button>
     </div>
   );
 }
